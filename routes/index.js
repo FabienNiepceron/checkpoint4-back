45cecc1cbf0237ec505e778dff4bf62b56bf0c9b
FabienNiepceron <fabienniepceron72@gmail.com>
2021-02-03 14:32:42 +0100

    creation des routes billeterie, galerie,login et tresorerie

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,12 @@
 const router = require("express").Router();
-const exempleRoutes = require("./exemple");
+const tresorerieRoutes = require("./tresorerie.routes");
+const galerieRoutes = require("./galerie.routes");
+const billeterieRoutes = require("./billeterie.routes");
+const loginRoutes = require("./login.routes");
 
-router.use("/exemple", exempleRoutes);
+router.use("/tresorerie", tresorerieRoutes);
+router.use("/galerie", galerieRoutes);
+router.use("/billeterie", billeterieRoutes);
+router.use("/login", loginRoutes);
 
 module.exports = router;
---
+++ routes/tresorerie.routes.js
@@ -0,0 +1,41 @@
+const router = require("express").Router();
+const connection = require("../db_connection");
+
+router.get("/", (_, res) => {
+  const sql = "SELECT idtresorie, type, valeur FROM tresorie";
+  connection.query(sql, (err, result) => {
+    if (err) {
+      res.status(500).json({ errorMessage: err.message });
+    } else {
+      res.status(200).json(result);
+    }
+  });
+});
+
+router.post("/", (req, res) => {
+  const tresorerie = req.body;
+  const sql = "INSERT INTO tresorie (name, type, valeur) VALUES (?, ?, ?)";
+  const { name, type, valeur } = tresorerie;
+  connection.query(sql, [name, type, valeur], (err, result) => {
+    if (err) {
+      res.status(500).json({ errorMessage: err.message });
+    } else {
+      res.status(200).json(result);
+    }
+  });
+});
+
+router.put("/", (req, res) => {
+  const tresorerie = req.body;
+  const { id } = req.params;
+  const sql = `UPDATE tresorie SET ? WHERE id = ${id}`;
+  connection.query(sql, [tresorerie, req.params.id], (err, result) => {
+    if (err) {
+      res.status(500).json({ errorMessage: err.message });
+    } else {
+      res.status(200).json(result);
+    }
+  });
+});
+
+module.exports = router;
